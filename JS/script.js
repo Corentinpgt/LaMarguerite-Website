ffb9b4944c7399f922b1c9654cf73b75cc82feca
Corentinpgt <corentin.pouget.19@gmail.com>
2024-05-03 12:14:44 +0200

    page d'accueil v1

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     $("#carouselExample").owlCarousel({
         items: 3,
         loop: true,
-        margin: 10,
+        margin: 0,
         dots : false,
         nav: true,
         navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
         slideBy: 1, // Nombre d'items à déplacer lors de la navigation
         autoplay: true, // Active le défilement automatique
-        autoplayTimeout: 2000, // Définit le délai de défilement automatique à 2 secondes
+        autoplayTimeout: 3000, // Définit le délai de défilement automatique à 2 secondes
         responsive: {
             0: {
                 items: 1
@@ -29,5 +29,5 @@ $(document).ready(function(){
 
 window.addEventListener('scroll', function() {
     var header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+    header.classList.toggle('sticky', window.scrollY >= 50);
 });
